288e207650fd8cd83b1d4f3b13d14d44abfb8bce
Rafael Teixeira de Lima <rtdl@ibm.com>
2025-01-17 09:07:48 -0600

    Add unicode to latex and test file

---
+++ mytest.py
@@ -0,0 +1,50 @@
+import lxml.etree as ET
+from dwml.omml import oMath2Latex
+
+repl = "{0}"
+
+OMML_NS = "{http://schemas.openxmlformats.org/officeDocument/2006/math}"
+
+docx_xml_path = "/data/wordscape_v2/test/document.xml"
+
+with open(docx_xml_path, "rb") as xml_file:
+    xml = xml_file.read()
+
+# print(xml)
+root = ET.fromstring(xml)
+# print(root)
+# print(root.tag)
+# print(root.find("*"))
+for child in root.iter():
+    if "MathPara" not in child.tag:
+        continue
+    for child2 in child:
+        if "Math" not in child2.tag:
+            continue
+        print(oMath2Latex(child2))
+        # print(child2.tag)
+# for omath in root.findall("*Math*"):
+# print(omath.tag)
+# for omath2 in omath.findall("*"):
+#     print(omath2.tag)
+#     for omath3 in omath2.findall("*"):
+#         print(omath3.tag)
+#         if "oMath" in omath3.tag:
+#             print(oMath2Latex(omath3))
+#             for omath4 in omath3.findall("*"):
+#                 if "oMath" in omath4.tag:
+#                     # print(omath4.to_string())
+#                     print(oMath2Latex(omath4))
+#                     print(ET.tostring(omath4))
+
+# print()
+# print()
+
+# print(DOCXML_ROOT.format(xml.group(0)))
+# print(omath_re.groups)
+# for x in omml.load_string(xml):
+#     print(x)
+
+# t = omath_re.sub(lambda m: _latex_fn(m, repl), xml)
+
+# print(t)
